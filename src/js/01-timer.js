284b041acbcb7cc9e8826d100393f5806ca254de
Altriana <150026999+Altriana@users.noreply.github.com>
2024-10-07 00:00:08 +0200

    Timer

---
+++ src/js/01-timer.js
@@ -0,0 +1,58 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+const input = document.querySelector("#datetime-picker");
+const btnStart = document.querySelector("[data-start]");
+const spanDays = document.querySelector("[data-days]");
+const spanHours = document.querySelector("[data-hours]");
+const spanMinutes = document.querySelector("[data-minutes]");
+const spanSeconds = document.querySelector("[data-seconds]");
+
+
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        const selectedDate = selectedDates[0];
+        if (selectedDate < new Date()) {
+            iziToast.warning({
+                title: 'Error',
+                message: 'Please choose a date in the future',
+            });
+            btnStart.disabled = true;
+        } else {
+            btnStart.disabled = false;
+        };
+    },
+};
+
+flatpickr(input, options);  
+
+
+
+
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
